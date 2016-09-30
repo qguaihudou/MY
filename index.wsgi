0c8044600978780ebfb4d9aec85f3553ac681952
unknown <>
2016-09-30 20:20:25 +0800

    updatefirst

---
+++ index.wsgi
@@ -0,0 +1,4 @@
+import sae
+sae.add_vendor_dir('site-packages')
+from main import app
+application=sae.create_wsgi_app(app)
---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def hello():
+    return "this is my blog!"
+
+if __name__=="__main__":
+    app.run(host = '127.0.0.1',port = 80,debug = True)
+    
